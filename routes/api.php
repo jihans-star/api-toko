c996c68d216da86780724800c7066abdd7cd12a5
jihans-star <jihancian@gmail.com>
2026-05-10 21:13:02 +0700

    clear cache

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\Api\ProductController;
 
-Route::post('/login', function(Request $request){
-
-    $credentials = $request->only('email','password');
-
-    if(Auth::attempt($credentials)){
-        return response()->json([
-            'message' => 'Login berhasil'
-        ]);
-    }
-
+Route::get('/test', function () {
     return response()->json([
-        'message' => 'Login gagal',
-        'data' => $credentials
-    ],401);
+        'status' => 'api works'
+    ]);
 });
-
-Route::apiResource('products', ProductController::class);
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'API TOKO RUNNING';
+    return response()->json([
+        'status' => 'laravel running'
+    ]);
 });
