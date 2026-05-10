35638def898df3c06c3afb896759459aae079115
jihans-star <jihancian@gmail.com>
2026-05-10 20:54:03 +0700

    aduh

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -9,37 +9,9 @@ use App\Models\Product;
 class ProductController extends Controller
 {
     public function index()
-    {
-        return Product::all();
-    }
-
-    public function store(Request $request)
-    {
-        $product = Product::create($request->all());
-
-        return response()->json($product);
-    }
-
-    public function show($id)
-    {
-        return Product::findOrFail($id);
-    }
-
-    public function update(Request $request, $id)
-    {
-        $product = Product::findOrFail($id);
-
-        $product->update($request->all());
-
-        return response()->json($product);
-    }
-
-    public function destroy($id)
-    {
-        Product::destroy($id);
-
-        return response()->json([
-            'message' => 'Deleted'
-        ]);
-    }
+{
+    return response()->json([
+        'message' => 'route works'
+    ]);
+}
 }
