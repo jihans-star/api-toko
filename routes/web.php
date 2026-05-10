80764048953124f1b7aee7dcb81b1f8d4c07c2e5
jihans-star <jihancian@gmail.com>
2026-05-10 20:48:26 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\ProductController;
+use Illuminate\Support\Facades\Route;
 
-Route::apiResource('products', ProductController::class);
+Route::get('/', function () {
+    return 'API TOKO RUNNING';
+});
